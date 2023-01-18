1f1c42dcba449e4b455884c2f8a3413109c36b23
Dmitriy <maradonna05@yandex.ru>
2023-01-18 11:46:34 +0300

    Добавьте файлы проекта.

---
+++ CICLE_FOR/CICLE_FOR.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	//	Задание
+
+	//	Создать программу, используя цикл for, которая с помощью цикла 
+	//	суммирует числа от 1 до 100.
+	int sum = 0;
+	for (int i = 1;i <= 100;i++)
+	{
+		printf("%i + %i = %i\n", sum, i, sum + i);
+		sum = sum + i;
+	}
+	printf("\n");
+	printf("Сумма чисел от 1 до 100 равна %i\n", sum);
+}
